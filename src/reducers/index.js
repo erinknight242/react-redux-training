ecc9c302fe28611d4825c915e92622d66c07d047
erinknight <erin.knight@headspring.com>
2018-04-16 13:43:52 -0500

    Update root reducer to combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import welcomeCard from '../reducers/welcomeCardReducer';
+import cardList from '../reducers/cardListReducer';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  welcomeCard,
+  cardList,
+});
 
 export default rootReducer;
