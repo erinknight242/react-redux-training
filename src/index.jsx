51d385e1a56e1fb77af61e8d710c6d216a01356b
erinknight <erin.knight@headspring.com>
2018-04-16 13:46:45 -0500

    Remove hard-coded data from index.jsx, and update it to include the Provider and store

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
 import 'material-design-lite/material.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import StaticDashboard from './components/StaticDashboard';
-
-// Static data
-const testData = {
-  welcomeMsg: {
-    title: 'Welcome to the Sample Dashboard',
-    msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus vitae velit a congue.',
-    imgUrl: 'http://icdn8.digitaltrends.com/image/funny-grumpy-cat-images-hd-wallpaper-1080x611-640x0.jpg',
-  },
-  cards: [
-    { id: 1, title: 'Foo', msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus vitae velit a congue.', imgUrl: 'https://picsum.photos/500/300' },
-    { id: 2, title: 'Bar', msg: 'Etiam pretium a elit in varius. Ut vel est accumsan, interdum erat sit amet, lacinia risus.', imgUrl: 'https://picsum.photos/500/300/?random' },
-  ],
-};
+import store from './store';
 
 ReactDOM.render(
-  <StaticDashboard {...testData} />,
+  <Provider store={store}>
+    <StaticDashboard />
+  </Provider>,
   document.getElementById('react-app'),
 );
